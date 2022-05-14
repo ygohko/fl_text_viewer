57711407e0a4e09a0363e015fd2f0f3c5b67093d
Yasuaki Gohko <ygohko@gmail.com>
2022-05-14 11:09:48 +0900

    Cleaned up.

--- lib/main.dart
+++ lib/main.dart
@@ -16,15 +16,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'FL Text Viewer'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -37,7 +35,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("FL Text Viewer"),
       ),
       body: Center(
         child: Column(
